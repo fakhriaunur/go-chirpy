bc2912d85916cdd753f4421c40debf160deeb695
fakhriaunur <fakhri.aunur>
2023-11-23 23:51:11 +0700

    serving index.html and logo.png with FileServer

--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
+	const port = "8080"
+	const filepathRoot = "."
+
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 	corsMux := middlewareCors(mux)
 
 	server := &http.Server{
-		Addr:    "localhost:8080",
+		Addr:    ":" + port,
 		Handler: corsMux,
 	}
-	server.ListenAndServe()
+
+	log.Printf("Serving files from %s on port: %s", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
 }
 
 func middlewareCors(next http.Handler) http.Handler {
