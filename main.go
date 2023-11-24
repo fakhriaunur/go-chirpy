37387d10cad65dba304878cd7124d08d6a3e829e
fakhriaunur <fakhri.aunur>
2023-11-24 15:16:38 +0700

    making shorthand aca access-control-allow

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/healthz", http.FileServer(http.Dir(filepathRoot)))
 	corsMux := middlewareCors(mux)
 
 	server := &http.Server{
@@ -24,9 +26,10 @@ func main() {
 
 func middlewareCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
+		const aca = "Access-Control-Allow"
+		w.Header().Set(aca+"-Origin", "*")
+		w.Header().Set(aca+"-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set(aca+"-Headers", "*")
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
