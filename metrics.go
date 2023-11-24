69860042856fb7f548e9e1257dd54538ebd86bba
fakhriaunur <fakhri.aunur>
2023-11-24 23:02:29 +0700

    switch api/metrics to admin/metrics, add html snippet in the admin/metrics response body

--- metrics.go
+++ metrics.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"strconv"
 )
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -15,6 +15,18 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits: " + strconv.Itoa(cfg.fileserverHits)))
+	// w.Write([]byte(fmt.Sprintln("Welcome, Chirpy Admin")))
+	// w.Write([]byte(fmt.Sprintf("Chirpy has been visited %d times!", cfg.fileserverHits)))
+	w.Write([]byte(fmt.Sprintf(`
+	<html>
+	
+	<body>
+		<h1>Welcome, Chirpy Admin</h1>
+		<p>Chirpy has been visited %d times!</p>
+	</body>
+	
+	</html>
+			`, cfg.fileserverHits)))
 }
